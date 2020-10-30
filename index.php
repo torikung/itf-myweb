544b9a00c936acd8e489feaadd365bbe194a741c
torikung <71125844+torikung@users.noreply.github.com>
2020-10-30 17:55:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,26 @@
-<?php
-echo "<h2>IT@KMITL</h2>";
-echo "Hello IT<br>";
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8"
+    <title>Something funny but not funny</title>
+  </head>
+  <body>
+    <h1>"Meme"</h1>
+    <img src="maf.jpg">
+    <img src="bro.jpg">
+    <img src="red.jpg">
+    <br>
+    
+    <a href="https://www.reddit.com/r/memes/"></a>
+    
+    <p> ณ ร้านขายหนังสือ ชายนิรนามคนหนึ่ง ถามหาหนังสือเล่มหนึ่งกับพนักงานสาวสวย
+
+“น้องครับ หนังสือ วิธีปราบเมีย อยู่ตรงไหนครับ?”
+
+พนักงานสาวเงยหน้ามามองชายหนุ่ม นึกอยู่สักครู่ จึงตอบกลับไปว่า
+
+“หนังสือ วิธีปราบเมีย อยู่ที่ โซน นิยาย จินตนาการ และเพ้อฝัน ค่ะ วางอยู่ใกล้หนังสือตายแล้วไปไหน แนะนำให้ซื้อคู่กันค่ะ” </p>
+    
+    <h2>kasipeemchad srisumrit, peem</h2>
+    </body>
+</html>
