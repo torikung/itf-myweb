0c95b80ebba4dec3b08fb556054e887e633c8f4b
torikung <71125844+torikung@users.noreply.github.com>
2020-11-13 18:36:04 +0700

    Update show.php

--- show.php
+++ show.php
@@ -16,7 +16,7 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
   <tr>
     <th width="100"> <div align="center">Name</div></th>
     <th width="350"> <div align="center">Comment </div></th>
-    <th width="150"> <div align="center">Link </div></th>
+    <th width="150"> <div align="center">Action </div></th>
   </tr>
 <?php
 while($Result = mysqli_fetch_array($res))
@@ -25,7 +25,14 @@ while($Result = mysqli_fetch_array($res))
   <tr>
     <td><?php echo $Result['name'];?></div></td>
     <td><?php echo $Result['comment'];?></td>
-    <td><?php echo $Result['link'];?></td>
+    <p>
+        <input type="submit" name="submit" value="del">
+    </p>
+    <p>
+        <input type="submit" name="submit" value="fix">
+    </p>
+    </form>
+</form>
   </tr>
 <?php
 }
