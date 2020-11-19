58e3dbccc2530889c3b58b5f1b5248c2dc3ae375
torikung <71125844+torikung@users.noreply.github.com>
2020-11-19 22:45:16 +0700

    Update show.php

--- show.php
+++ show.php
@@ -10,18 +10,18 @@
 </head>
 <body>
 <?php
-$conn = mysqli_init();
-mysqli_real_connect($conn, 'itfdatabest.mysql.database.azure.com', 'torikung@itfdatabest', 'itf5544!', 'itflab', 3306);
-if (mysqli_connect_errno($conn))
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
-$res = mysqli_query($conn, 'SELECT * FROM guestbook');
+    $conn = mysqli_init();
+    mysqli_real_connect($conn, 'itfraiden.mysql.database.azure.com', 'raiden@itfraiden', 'Itf12644', 'itf63070222', 3306);
+    if (mysqli_connect_errno($conn))
+    {
+        die('Failed to connect to MySQL: '.mysqli_connect_error());
+    }
+    $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
 <div class="container">
     <div align="center" class="text-primary"><h1>DataBase Table</h1></div>
     <table width="400" border="1">
-        <table class="table table-sm table-dark table-striped table-bordered">
+        <table class="table table-dark table-striped table-bordered">
             <thead>
                 <tr>
                     <th width="100"> <div align="center" class="p-3 mb-2 bg-primary text-white">Name</div></th>
@@ -34,31 +34,30 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
     <?php
         while($Result = mysqli_fetch_array($res))
         {
-?>
-  <tr>
-    <td><?php echo $Result['name'];?></div></td>
-    <td><?php echo $Result['comment'];?></td>
-    <td><?php echo $Result['Link'];?></td>
-    <td><div align="center">
-            <form action="edit_form.php" method="post">
-                <input type="hidden" name="ID" value=<?php echo $Result['ID'];?>>
-                <button type="submit" class="btn btn-light">แก้ไข</button></form></div>
-    </td>
-    <td><div align="center">
-            <form action="delete.php" method="post">
-                <input type="hidden" name="ID" value=<?php echo $Result['ID'];?>>
-                <button type="submit" class="btn btn-light">ลบ</button></form></div>
-    </td>
-  </tr>
-<?php
-}
-?>
-</table>
+    ?>
+            <td><?php echo $Result['Name'];?></div></td>
+            <td><?php echo $Result['Comment'];?></td>
+            <td><?php echo $Result['Link'];?></td>
+            <td><div align="center">
+                    <form action="edit_form.php" method="post">
+                        <input type="hidden" name="ID" value=<?php echo $Result['ID'];?>>
+                        <button type="submit" class="btn btn-light">แก้ไข</button></form></div>
+            </td>
+            <td><div align="center">
+                    <form action="delete.php" method="post">
+                        <input type="hidden" name="ID" value=<?php echo $Result['ID'];?>>
+                        <button type="submit" class="btn btn-light">ลบ</button></form></div>
+            </td>
+        </tr>
+    <?php
+        }
+    ?>
+    </table>
 <div class="container">
     <form action="form.html" method="POST"
-    <p>
-        <div align="center"><input type="submit" class="btn btn-dark" name="submit" value="เพิ่ม"></div>
-    </p>
+        <p>
+            <div align="center"><input type="submit" class="btn btn-dark" name="submit" value="เพิ่ม"></div>
+        </p>
     </form>
 </div>
 <?php
