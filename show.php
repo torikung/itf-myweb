aae4173484e8e1447facd1a1f2de2e43b77a1f92
torikung <71125844+torikung@users.noreply.github.com>
2020-11-20 02:40:07 +0700

    Update show.php

--- show.php
+++ show.php
@@ -7,11 +7,17 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <form style ="background-image: url('https://wallpaperaccess.com/full/31367.jpg');"> 
-    <input required type="text" name="name" /> 
-    </form> 
+    <style>
+        .formbg { 
+            background-color: white; 
+            background-image: url(''); 
+            background-size: auto; 
+            background-repeat: repeat; 
+            background-attachment: scroll; 
+        } 
+    </style>
 </head>
-<body>
+<body class=”formbg”> 
 <?php
     $conn = mysqli_init();
     mysqli_real_connect($conn, 'itfdatabest.mysql.database.azure.com', 'torikung@itfdatabest', 'itf5544!', 'itflab', 3306);
@@ -65,8 +71,5 @@
 <?php
 mysqli_close($conn);
 ?>
-<form style ="background-image: url('https://wallpaperaccess.com/full/31367.jpg');"> 
-<input required type="text" name="name" /> 
-</form> 
 </body>
 </html>
