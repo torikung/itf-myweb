4bb05e10b2f5f31a4bf27de393c731639e902512
torikung <71125844+torikung@users.noreply.github.com>
2020-11-19 20:42:46 +0700

    Update show.php

--- show.php
+++ show.php
@@ -10,15 +10,39 @@ if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-$res = mysqli_query($conn, 'SELECT * FROM guestbook');
+<?php 
+        if(isset($_SESSION["id"])){
+        $editt = "SELECT * FROM guestbook WHERE ID='".$_SESSION["id"]."'";
+        $res = mysqli_query($conn, $editt);
+        $Reedit = mysqli_fetch_array($res)
+        ?>
+            
+        <form class="form" action="" method="post">
+            <div style="opacity:0%;">.</div>
+            Name:<br>
+                <input class="in" type="text" name = "ename" id="idName" placeholder="Enter Name" value="<?php echo $Reedit['Name'];?>"><br>
+            Comment:<br>
+            <textarea class="in"rows="10" cols="20" name = "ecomment" id="idComment" placeholder="Enter Comment"><?php echo $Reedit['Comment'];?></textarea><br>
+            Link:<br>
+            <input class="in" type="text" name = "elink" id="idLink" placeholder="Enter Link" value="<?php echo $Reedit['Link'];?>"><br><br>
+            <button class="btn" type="submit" name="editBtn">Submit</button><br><br>
+            <div style="opacity:0%;">.</div>
+        </form>
+        
+        <?php
+        }else{
+        if(isset($_SESSION['dire'])){
+if($_SESSION['dire'] == "View"){
 ?>
 <table width="600" border="1">
   <tr>
     <th width="100"> <div align="center">Name</div></th>
     <th width="350"> <div align="center">Comment </div></th>
+    <th width="150"> <div align="center">Link </div></th>
     <th width="150"> <div align="center">Action </div></th>
   </tr>
 <?php
+$res = mysqli_query($conn, 'SELECT * FROM guestbook');
 while($Result = mysqli_fetch_array($res))
 {
 ?>
@@ -45,5 +69,7 @@ mysqli_close($conn);
         <input type="submit" name="submit" value="เพิ่ม">
     </p>
 </form>
+}
+}
 </body>
 </html>
