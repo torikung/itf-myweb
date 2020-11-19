e31721b9460280187203b8206c98b2d8866c2ecf
torikung <71125844+torikung@users.noreply.github.com>
2020-11-19 22:54:00 +0700

    Update show.php

--- show.php
+++ show.php
@@ -11,7 +11,7 @@
 <body>
 <?php
     $conn = mysqli_init();
-    mysqli_real_connect($conn, 'itfraiden.mysql.database.azure.com', 'raiden@itfraiden', 'Itf12644', 'itf63070222', 3306);
+    mysqli_real_connect($conn, 'itfdatabest.mysql.database.azure.com', 'torikung@itfdatabest', 'itf5544!', 'itflab', 3306);
     if (mysqli_connect_errno($conn))
     {
         die('Failed to connect to MySQL: '.mysqli_connect_error());
